b6a60e6bdcc704bd39ac3e4218d27d6e2bf75e09
wuzhengtao <zhengtao.wuzt@gmail.com>
2017-03-21 16:05:40 +0800

    split

---
+++ start-consul.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+LOCAL_IP=$(ifconfig eth0 | grep inet | awk '{{print $2}}')
+
+HOST_IP=${HOST_IP:-$LOCAL_IP}
+ZK_URL=${ZK_URL:-"zk://${HOST_IP}:2181/default"}
+
+
+docker -H unix:///var/run/bootstrap.sock run -ti --rm \
+        -v $(pwd):$(pwd) \
+	    -v /var/run/docker.sock:/var/run/docker.sock \
+        -e DOCKER_HOST=unix:///var/run/docker.sock  \
+        -e HOST_IP=${HOST_IP} \
+        -e LOCAL_IP=${LOCAL_IP} \
+        -e ZK_URL=${ZK_URL} \
+        -w $(pwd)  docker/compose:1.9.0 \
+        -f compose/consul.yml \
+        -p consul \
+        up -d server mesos-consul
--- start-docker.sh
+++ start-docker.sh
@@ -32,27 +32,4 @@ systemctl status docker -l
 
 
 
-docker -H unix:///var/run/bootstrap.sock run -ti --rm \
-        -v $(pwd):$(pwd) \
-	    -v /var/run/docker.sock:/var/run/docker.sock \
-        -e DOCKER_HOST=unix:///var/run/docker.sock  \
-        -e HOST_IP=${HOST_IP} \
-        -e ZK_URL=${ZK_URL} \
-        -w $(pwd)  docker/compose:1.9.0 \
-        -f compose/mesos.yml \
-        -p mesos \
-        up -d
 
-
-
-docker -H unix:///var/run/bootstrap.sock run -ti --rm \
-        -v $(pwd):$(pwd) \
-	    -v /var/run/docker.sock:/var/run/docker.sock \
-        -e DOCKER_HOST=unix:///var/run/docker.sock  \
-        -e HOST_IP=${HOST_IP} \
-        -e LOCAL_IP=${LOCAL_IP} \
-        -e ZK_URL=${ZK_URL} \
-        -w $(pwd)  docker/compose:1.9.0 \
-        -f compose/consul.yml \
-        -p consul \
-        up -d server mesos-consul

3a83aa6b4d4027c441e6f5f278dc1c987d5e7e5d
wuzhengtao <zhengtao.wuzt@gmail.com>
2017-03-21 17:29:20 +0800

    local_ip host_ip

--- start-mesos.sh
+++ start-mesos.sh
@@ -12,7 +12,7 @@ docker -H unix:///var/run/bootstrap.sock run -ti --rm \
         -v $(pwd):$(pwd) \
 	    -v /var/run/docker.sock:/var/run/docker.sock \
         -e DOCKER_HOST=unix:///var/run/docker.sock  \
-        -e HOST_IP=${HOST_IP} \
+        -e LOCAL_IP=${LOCAL_IP} \
         -e ZK_URL=${ZK_URL} \
         -w $(pwd)  docker/compose:1.9.0 \
         -f compose/mesos.yml \

bdc16f0ad4d9a83bc7f5c0d57752371ac33c5996
uniseraph <zhengtao.wuzt@gmail.com>
2017-03-22 22:40:55 +0800

    install docker tgz

--- init-node.sh
+++ init-node.sh
@@ -3,9 +3,14 @@
 yum install -y docker jq
 
 
-cp -f bootstrap/bootstrap.service /etc/systemd/system/
-cp -f bootstrap/bootstrap.socket /etc/systemd/system/
-cp -f bootstrap/bootstrap /etc/sysconfig/bootstrap
+cp -f systemd/bootstrap/bootstrap.service /etc/systemd/system/
+#cp -f systemd/bootstrap/bootstrap.socket /etc/systemd/system/
+cp -f systemd/bootstrap/bootstrap /etc/sysconfig/bootstrap
+
+
+ rm -rf /tmp/docker-1.12.6.tgz && wget -o /tmp/docker-1.12.6.tgz https://get.docker.com/builds/Linux/x86_64/docker-1.12.6.tgz
+
+tar zxvf /tmp/docker-1.12.6 && cp -rf /tmp/docker-1.12.6/* /usr/bin/
 
 
 systemctl start bootstrap

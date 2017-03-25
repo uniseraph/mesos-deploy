8343240c9da24db3176bb22cb5f93d3be05d4726
zhengtao.wuzt <zhengtao.wuzt@gmail.com>
2017-03-25 22:22:49 +0800

    using local docker

--- init-node.sh
+++ init-node.sh
@@ -9,9 +9,8 @@ cp -f systemd/bootstrap/bootstrap /etc/sysconfig/bootstrap
 
 cp -f systemd/dnsmasq/dnsmasq.service /usr/lib/systemd/system/dnsmasq.service
 
- rm -rf docker-1.12.6.tgz && wget https://get.docker.com/builds/Linux/x86_64/docker-1.12.6.tgz
 
-tar zxvf docker-1.12.6.tgz && cp -rf docker/* /usr/bin/  && rm -rf docker docker-1.12.6.tgz
+tar zxvf binary/docker-1.12.6.tgz && cp -rf docker/* /usr/bin/  && rm -rf docker 
 
 systemctl daemon-reload
 systemctl start bootstrap

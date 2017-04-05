d53693412712df80f23c9792b2386eb8d19aaf52
wuzhengtao <zhengtao.wuzt@gmail.com>
2017-04-05 10:18:39 +0800

    using zk cluster

--- start-mesos.sh
+++ start-mesos.sh
@@ -4,8 +4,8 @@
 
 LOCAL_IP=$(ifconfig eth0 | grep inet | awk '{{print $2}}')
 
-HOST_IP=${HOST_IP:-$LOCAL_IP}
-ZK_URL=${ZK_URL:-"zk://${HOST_IP}:2181"}
+
+ZK_URL=${ZK_URL:-"zk://${MASTER0_IP}:2181,${MASTER1_IP}:2181,${MASTER2_IP}:2181"}
 
 
 docker -H unix:///var/run/bootstrap.sock run -ti --rm \

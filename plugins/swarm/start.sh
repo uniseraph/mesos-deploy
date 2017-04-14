cffba346edad91db7d380c6c9badf784cc30e1c0
uniseraph <zhengtao.wuzt@gmail.com>
2017-04-15 00:50:35 +0800

    fixbug

--- plugins/swarm/start.sh
+++ plugins/swarm/start.sh
@@ -11,12 +11,12 @@ DIS_URL=${DIS_URL:-"zk://${MASTER0_IP}:2181,${MASTER1_IP}:2181,${MASTER2_IP}:218
 HOSTNAME=`hostname`
 
 docker run -ti --rm \
-        -v $(BASE_DIR):$(BASE_DIR) \
+        -v ${BASE_DIR}:${BASE_DIR} \
 	    -v /var/run/docker.sock:/var/run/docker.sock \
         -e DOCKER_HOST=unix:///var/run/docker.sock  \
         -e LOCAL_IP=${LOCAL_IP} \
         -e DIS_URL=${DIS_URL} \
-        -w $(BASE_DIR) \
+        -w ${BASE_DIR} \
         docker/compose:1.9.0 \
         -p swarm \
         up -d $*

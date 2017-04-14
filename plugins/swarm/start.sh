644bef33ee33c27de2fe7eff924197e19ce0ac25
uniseraph <zhengtao.wuzt@gmail.com>
2017-04-14 23:19:28 +0800

    bugfix

--- plugins/swarm/start.sh
+++ plugins/swarm/start.sh
@@ -5,18 +5,17 @@
 LOCAL_IP=$(ifconfig eth0 | grep inet | awk '{{print $2}}')
 
 
-DIS_URL=${ZK_URL:-"zk://${MASTER0_IP}:2181,${MASTER1_IP}:2181,${MASTER2_IP}:2181"}
+DIS_URL=${DIS_URL:-"zk://${MASTER0_IP}:2181,${MASTER1_IP}:2181,${MASTER2_IP}:2181/default"}
 
 HOSTNAME=`hostname`
 
-docker -H unix:///var/run/bootstrap.sock run -ti --rm \
+docker run -ti --rm \
         -v $(pwd):$(pwd) \
 	    -v /var/run/docker.sock:/var/run/docker.sock \
         -e DOCKER_HOST=unix:///var/run/docker.sock  \
         -e LOCAL_IP=${LOCAL_IP} \
         -e DIS_URL=${DIS_URL} \
-        -e HOSTNAME=${HOSTNAME} \
-        -w $(pwd) docker/compose:1.9.0 \
-        -f compose/mesos.yml \
-        -p mesos \
+        -w $(pwd) \
+        docker/compose:1.9.0 \
+        -p swarm \
         up -d $*

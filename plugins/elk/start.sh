2f085743e5af2a0c833ffc7476cc6748b330491c
wuzhengtao <zhengtao.wuzt@gmail.com>
2017-05-04 10:51:51 +0800

    add MASTER_IP env

--- plugins/elk/start.sh
+++ plugins/elk/start.sh
@@ -5,7 +5,7 @@ BASE_DIR=$(cd `dirname $0` && pwd -P)
 
 LOCAL_IP=$(ifconfig eth0 | grep inet | awk '{{print $2}}')
 
-
+MASTER_IP=${}
 #DIS_URL=${DIS_URL:-"zk://${MASTER0_IP}:2181,${MASTER1_IP}:2181,${MASTER2_IP}:2181/default"}
 
 #HOSTNAME=`hostname`
@@ -15,6 +15,7 @@ docker run -ti --rm \
 	    -v /var/run/docker.sock:/var/run/docker.sock \
         -e DOCKER_HOST=unix:///var/run/docker.sock  \
         -e LOCAL_IP=${LOCAL_IP} \
+        -e MASTER_IP=${MASTER_IP} \
         -w ${BASE_DIR} \
         docker/compose:1.9.0 \
         up -d $*

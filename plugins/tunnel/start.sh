82ee6fb033c41cbe54834a9ed28249e11d274075
wuzhengtao <zhengtao.wuzt@gmail.com>
2017-05-15 13:28:09 +0800

    add elk

---
+++ plugins/tunnel/start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(cd `dirname $0` && pwd -P)
+
+
+docker run -ti --rm \
+        -v ${BASE_DIR}:${BASE_DIR} \
+	    -v /var/run/docker.sock:/var/run/docker.sock \
+        -e DOCKER_HOST=${MASTER0_IP}:2375  \
+        -w ${BASE_DIR} \
+        docker/compose:1.9.0 \
+        -p swarm \
+        up -d $*

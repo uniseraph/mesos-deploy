56382717e430610f1adf64328d3b7cf608f93411
uniseraph <zhengtao.wuzt@gmail.com>
2017-05-16 00:16:33 +0800

    add zookeeper

---
+++ plugins/zookeeper/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+BASE_DIR=$(cd `dirname $0` && pwd -P)
+
+
+
+
+docker run -ti --rm \
+        -v ${BASE_DIR}:${BASE_DIR} \
+	    -v /var/run/docker.sock:/var/run/docker.sock \
+        -e DOCKER_HOST=unix:///var/run/docker.sock  \
+        -w ${BASE_DIR} \
+        docker/compose:1.9.0 \
+        up -d $*

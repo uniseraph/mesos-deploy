33b72c39308d102212abfbb0cf424f01b87ddef8
uniseraph <zhengtao.wuzt@gmail.com>
2017-05-22 23:58:05 +0800

    support ubuntu16.04

--- plugins/beats/start.sh
+++ plugins/beats/start.sh
@@ -6,7 +6,8 @@ BASE_DIR=$(cd `dirname $0` && pwd -P)
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.4.0-x86_64.rpm
 rpm -vi filebeat-5.4.0-x86_64.rpm && rm -rf filebeat-5.4.0-x86_64.rpm
 
-
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.4.0-amd64.deb
+dpkg -i filebeat-5.4.0-amd64.deb && rm -rf filebeat-5.4.0-amd64.deb
 
 cp ${BASE_DIR}/filebeat/config/filebeat.yml /etc/filebeat/filebeat.yml
 sed -i -e "s#master#${MASTER_IP}#g" /etc/filebeat/filebeat.yml
@@ -14,8 +15,10 @@ systemctl restart filebeat
 systemctl enable filebeat
 systemctl status filebeat
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.4.0-x86_64.rpm
-rpm -vi metricbeat-5.4.0-x86_64.rpm && rm -rf metricbeat-5.4.0-x86_64.rpm
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.4.0-amd64.deb
+dpkg -i metricbeat-5.4.0-amd64.deb  && rm -rf metricbeat-5.4.0-amd64.deb
+
 
 cp ${BASE_DIR}/metricbeat/config/metricbeat.yml /etc/metricbeat/metricbeat.yml
 sed -i -e "s#master#${MASTER_IP}#g" /etc/metricbeat/metricbeat.yml

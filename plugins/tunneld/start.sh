fafdc18911f9a0876b66bc250523829c6f6ab6a6
uniseraph <zhengtao.wuzt@gmail.com>
2017-07-13 20:28:56 +0800

    add tcp for DOCKER_HOST

--- plugins/tunneld/start.sh
+++ plugins/tunneld/start.sh
@@ -6,7 +6,7 @@ BASE_DIR=$(cd `dirname $0` && pwd -P)
 docker run -ti --rm \
         -v ${BASE_DIR}:${BASE_DIR} \
 	    -v /var/run/docker.sock:/var/run/docker.sock \
-        -e DOCKER_HOST=${MASTER0_IP}:2375  \
+        -e DOCKER_HOST=tcp://${MASTER0_IP}:2375  \
         -w ${BASE_DIR} \
         docker/compose:1.9.0 \
         up -d $*

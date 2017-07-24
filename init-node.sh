6ac7ed5ac2fe0d845ec5815a4fdda0c37df7f4b3
uniseraph <zhengtao.wuzt@gmail.com>
2017-07-24 12:58:12 +0800

    first

--- init-node.sh
+++ init-node.sh
@@ -8,37 +8,39 @@ sysctl -w vm.max_map_count=262144
 cp sysctl.conf /etc/sysctl.conf
 sysctl -p
 
-yum install -y  jq bind-utils bridge-utils tcpdump dnsmasq haveged strace pstack htop iostat vmstat curl wget sysdig pidstat mpstat iotop blktrace perf  dstat ltrace lsof
+#yum install -y  jq bind-utils bridge-utils tcpdump dnsmasq haveged strace pstack htop iostat vmstat curl wget sysdig pidstat mpstat iotop blktrace perf  dstat ltrace lsof
+apt-get install -y jq  bridge-utils tcpdump dnsmasq haveged strace pstack htop  curl wget  iotop blktrace   dstat ltrace lsof
 
-rpm -i binary/zanecloud-docker-1.11.1-d349391.x86_64.rpm
+#rpm -i binary/zanecloud-docker-1.11.1-d349391.x86_64.rpm
 #systemctl enable haveged
 #systemctl restart haveged
 
+rm -rf d349391 && mkdir -p d349391 && cd d349391
+wget http://zanecloud-docker.oss-cn-shanghai.aliyuncs.com/1.11.1/d349391/docker-1.11.1
+wget http://zanecloud-docker.oss-cn-shanghai.aliyuncs.com/1.11.1/d349391/docker-containerd
+wget http://zanecloud-docker.oss-cn-shanghai.aliyuncs.com/1.11.1/d349391/docker-containerd-ctr
+wget http://zanecloud-docker.oss-cn-shanghai.aliyuncs.com/1.11.1/d349391/docker-containerd-shim
+wget http://zanecloud-docker.oss-cn-shanghai.aliyuncs.com/1.11.1/d349391/docker-runc
+
+cd ..
+sudo cp -f  d349391/* /usr/bin/
+ln -s /usr/bin/docker-1.11.1 /usr/bin/docker
+
+
+
+cp -f systemd/docker-1.11/docker.service /etc/systemd/system/
+cp -f systemd/docker-1.11/docker.socket /etc/systemd/system/
+
 cp -f systemd/bootstrap/bootstrap.service /etc/systemd/system/
 cp -f systemd/bootstrap/bootstrap.socket /etc/systemd/system/
-#cp -f systemd/bootstrap/bootstrap-cleanup.timer /etc/systemd/system/
 cp -f systemd/bootstrap/bootstrap /etc/sysconfig/bootstrap
 
-cp -f systemd/dnsmasq/dnsmasq.service /usr/lib/systemd/system/dnsmasq.service
+#cp -f systemd/dnsmasq/dnsmasq.service /usr/lib/systemd/system/dnsmasq.service
 
 
-#tar zxvf binary/docker-1.12.6.tgz && cp -rf docker/* /usr/bin/  && rm -rf docker
 
 systemctl daemon-reload
 systemctl start bootstrap
 systemctl enable bootstrap
 systemctl status bootstrap -l
 
-#pip install --upgrade pip
-#pip install docker-compose
-
-#pip install -U pip setuptools
-#pip install docker-py
-#curl -L https://bit.ly/glances | /bin/bash
-
-#mkdir /etc/glances
-#cp -f systemd/glances/glances.service /etc/systemd/system/
-#cp -f systemd/glances/glances.etc  /etc/glances/glances.etc
-
-#systemctl start dnsmasq
-#systemctl status dnsmasq -l

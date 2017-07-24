364d3e9df724367e6245f346c5a60b5a904c180b
uniseraph <zhengtao.wuzt@gmail.com>
2017-07-24 17:18:36 +0800

    fix

--- start-docker.sh
+++ start-docker.sh
@@ -24,7 +24,7 @@ systemctl stop docker
 #echo '# /etc/sysconfig/docker-network'  > /etc/sysconfig/docker-network
 #echo "DOCKER_NETWORK_OPTIONS=\" -H unix:///var/run/docker.sock -H ${LOCAL_IP}:2376  --dns ${LOCAL_IP}  --ip-masq=${FLANNEL_IPMASQ}  --bip=${FLANNEL_SUBNET} --mtu=${FLANNEL_MTU}  --registry-mirror=https://rmw18jx4.mirror.aliyuncs.com  \""  >> /etc/sysconfig/docker-network
 
-echo "DOCKER_OPTS=\"  --dns ${LOCAL_IP}  --ip-masq=${FLANNEL_IPMASQ}  --bip=${FLANNEL_SUBNET} --mtu=${FLANNEL_MTU} --log-driver=json-file --log-opt max-file=10 --log-opt max-size=100m -s overlay --registry-mirror=https://rmw18jx4.mirror.aliyuncs.com  \""  >> /etc/default/docker
+echo "DOCKER_OPTS=\"  --dns ${LOCAL_IP}  --ip-masq=${FLANNEL_IPMASQ}  --bip=${FLANNEL_SUBNET} --mtu=${FLANNEL_MTU} --log-driver=json-file --log-opt max-file=10 --log-opt max-size=100m -s overlay --registry-mirror=https://rmw18jx4.mirror.aliyuncs.com  \""  >> /etc/systconfig/docker
 
 #aws需要，否则容器无法ping宿主机
 iptables -t nat  -A POSTROUTING -o eth0 -s ${FLANNEL_SUBNET}  -j MASQUERADE

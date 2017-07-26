74cbbcad8c18f3e5eb8c2659cc8139aad136dec7
uniseraph <zhengtao.wuzt@gmail.com>
2017-07-26 20:37:16 +0800

    fix

--- init-node.sh
+++ init-node.sh
@@ -8,20 +8,6 @@ sysctl -w vm.max_map_count=262144
 cp sysctl.conf /etc/sysctl.conf
 sysctl -p
 
-if type apt-get >/dev/null 2>&1; then
-  echo 'using apt-get '
-  sudo apt-get update && apt-get install -y jq  bridge-utils tcpdump  haveged strace pstack htop  curl wget  iotop blktrace   dstat ltrace lsof
-  export LOCAL_IP=$(ifconfig eth0 | grep inet\ addr | awk '{print $2}' | awk -F: '{print $2}')
-
-elif type yum >/dev/nul 2>&1; then
-  echo 'using yum'
-  sudo yum install -y  jq bind-utils bridge-utils tcpdump dnsmasq haveged strace pstack htop iostat vmstat curl wget sysdig pidstat mpstat iotop blktrace perf  dstat ltrace lsof
-  export LOCAL_IP=$(ifconfig eth0 | grep inet | awk '{{print $2}}')
-
-else
-  echo "no apt-get and no yum, exit"
-  exit
-fi
 
 
 

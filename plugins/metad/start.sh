20fedb92aaf71a3074a3d7b7ffbdbe629d51c336
uniseraph <zhengtao.wuzt@gmail.com>
2017-08-01 11:24:08 +0800

    fix

--- plugins/metad/start.sh
+++ plugins/metad/start.sh
@@ -9,7 +9,7 @@ BASE_DIR=$(cd `dirname $0` && pwd -P)
 docker run --net=host -ti --rm \
         -v ${BASE_DIR}:${BASE_DIR} \
 	    -v /var/run/docker.sock:/var/run/docker.sock \
-        -e DOCKER_HOST=${MASTER_IP}:2375  \
+        -e DOCKER_HOST=unix:///var/run/docker.sock \
         -w ${BASE_DIR} \
         docker/compose:1.9.0 \
         up -d $*

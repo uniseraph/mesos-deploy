0048e4ea249ce2e3161000d1d3051636fd6299b3
uniseraph <zhengtao.wuzt@gmail.com>
2017-08-04 12:09:39 +0800

    support centos and ubuntu

--- plugins/beats/start.sh
+++ plugins/beats/start.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
-
-
 BASE_DIR=$(cd `dirname $0` && pwd -P)
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.4.0-x86_64.rpm
-rpm -vi filebeat-5.4.0-x86_64.rpm && rm -rf filebeat-5.4.0-x86_64.rpm
-
-
+if type dpkg >/dev/null 2>&1; then
+    curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.5.1-amd64.deb
+    sudo dpkg -i metricbeat-5.5.1-amd64.deb  &&  rm -f metricbeat-5.5.1-amd64.deb
+    curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.5.1-amd64.deb
+    sudo dpkg -i filebeat-5.5.1-amd64.deb && rm -f  filebeat-5.5.1-amd64.deb
+elif type rpm >/dev/null 2>&1; then
+    curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.5.1-x86_64.rpm
+    sudo rpm -vi metricbeat-5.5.1-x86_64.rpm  && rm -rf  metricbeat-5.5.1-x86_64.rpm
+    curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.4.0-x86_64.rpm
+    rpm -vi filebeat-5.4.0-x86_64.rpm && rm -rf filebeat-5.4.0-x86_64.rpm
+else
+    echo "no dpkg and no yum"
+    exit
+fi
 
 cp ${BASE_DIR}/filebeat/config/filebeat.yml /etc/filebeat/filebeat.yml
 sed -i -e "s#master0#${MASTER0_IP}#g" /etc/filebeat/filebeat.yml
@@ -14,14 +22,10 @@ sed -i -e "s#master1#${MASTER1_IP}#g" /etc/filebeat/filebeat.yml
 sed -i -e "s#master2#${MASTER2_IP}#g" /etc/filebeat/filebeat.yml
 systemctl restart filebeat
 systemctl enable filebeat
-
-
 systemctl status filebeat
 /usr/share/filebeat/scripts/import_dashboards -es http://${MASTER0_IP}:9200 -user elastic
 
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.4.0-x86_64.rpm
-rpm -vi metricbeat-5.4.0-x86_64.rpm && rm -rf metricbeat-5.4.0-x86_64.rpm
 
 cp ${BASE_DIR}/metricbeat/config/metricbeat.yml /etc/metricbeat/metricbeat.yml
 sed -i -e "s#master0#${MASTER0_IP}#g" /etc/metricbeat/metricbeat.yml
@@ -29,7 +33,5 @@ sed -i -e "s#master1#${MASTER1_IP}#g" /etc/metricbeat/metricbeat.yml
 sed -i -e "s#master2#${MASTER2_IP}#g" /etc/metricbeat/metricbeat.yml
 systemctl restart metricbeat
 systemctl enable metricbeat
-
-
 systemctl status metricbeat
 /usr/share/metricbeat/scripts/import_dashboards -es http://${MASTER0_IP}:9200 -user elastic
--- plugins/tunneld/start.sh
+++ plugins/tunneld/start.sh
@@ -12,17 +12,26 @@ docker run --net=host -ti --rm \
         docker/compose:1.9.0 \
         up -d $*
 
+
 if [[ ${PROVIDER} == "aws" ]]; then
-    PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+    TUNNELD_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+    if [[ -z ${TUNNELD_IP} ]]; then
+        TUNNELD_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+    fi
 elif  [[ ${PROVIDER} == "aliyun" ]]; then
-    PUBLIC_IP=$(curl http://100.100.100.200/latest/meta-data/eipv4)
+    TUNNELD_IP=$(curl http://100.100.100.200/latest/meta-data/eipv4)
+    if [[ -z ${TUNNELD_IP} ]]; then
+        TUNNELD_IP=$(curl http://100.100.100.200/latest/meta-data/local-ipv4)
+    fi
+elif  [[ ${PROVIDER} == "native" ]]; then
+    TUNNELD_IP=$(LOCAL_IP)
 else
    echo "no such provider ${PROVIDER}"
-    exit
+   exit
 fi
 
 cp -f plugins/tunneld/tunneld-service.json.template plugins/tunneld/tunneld-service.json
 
-sed -i -e "s#localhost#${PUBLIC_IP}#g" plugins/tunneld/tunneld-service.json
+sed -i -e "s#localhost#${TUNNELD_IP}#g" plugins/tunneld/tunneld-service.json
 
 curl -H "Content-Type: application/json" -X POST -d @plugins/tunneld/tunneld-service.json http://127.0.0.1:6400/services/create
